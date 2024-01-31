d84da65f5f18552816613491669c96b7035ff78d
thanh0132 <158223258+thanh0132@users.noreply.github.com>
2024-01-30 23:17:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function showMessage(response) {
     noButton.style.position = "absolute";
 
     // Change image source to "Wolf.jpg"
-    document.getElementsByClassName("image")[0].src = "images/Wolf.jpg";
+    document.getElementsByClassName("image")[0].src = "Wolf.jpg";
 
     // Generate random coordinates within the visible container
     const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
@@ -37,7 +37,7 @@ function showMessage(response) {
     yesMessage.textContent = "YAY! I knew you would choose the right answer <3. See you on the 14th!";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
-    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+    document.getElementsByClassName("image")[0].src = "dance.gif";
 
     // Remove yes button
     document.getElementById("yesButton").remove();
